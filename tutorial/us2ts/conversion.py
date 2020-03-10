ac1b06a0f8f2a1ebcc90548bbc0962b1312191d5
hsolbrig <solbrig@jhu.edu>
2020-03-10 11:46:32 -0500

    Add slides

--- tutorial/us2ts/conversion.py
+++ tutorial/us2ts/conversion.py
@@ -3,12 +3,13 @@ from typing import List, Dict
 from rdflib import Graph
 
 convert: Dict[str, List[str]] = {
-    "example1_step5.json": ["example1_step5.ttl"]
+    "example1_step5.json": ["example1_step5.ttl"],
+    "final.nt": ["final.ttl", "final.xml"]
 }
 
 
 for source, targets in convert.items():
     g = Graph()
-    g.load(source, format='json-ld' if source.endswith(".json") else None)
+    g.load(source, format='json-ld' if source.endswith(".json") else "n3" if source.endswith(".nt") else None)
     for target in targets:
-        g.serialize(target, format="turtle" if target.endswith(".ttl") else None)
+        g.serialize(target, format="turtle" if target.endswith(".ttl") else "xml")
